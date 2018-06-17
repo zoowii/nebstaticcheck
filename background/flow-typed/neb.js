0e582e22bc696fd8c26fbe824767f6b94ee4fa42
zoowii <1992zhouwei@gmail.com>
2018-06-18 01:07:55 +0800

    more neb contract inner apis

--- background/flow-typed/neb.js
+++ background/flow-typed/neb.js
@@ -1,5 +1,46 @@
 import BigNumber from 'bignumber.js';
 
-declare var LocalContractStorage: any;
-declare var Blockchain: any;
-declare var Event: any;
+
+interface LocalContractStorageInterface {
+    defineMapProperty: Function,
+    defineProperties: Function,
+    defineMapProperties: Function,
+    defineProperty: Function
+}
+
+interface TransactionInterface {
+    from: string,
+    to: string,
+    value: BigNumber,
+    hash: string,
+    nonce: number,
+    timestamp: number,
+    gasLimit: number,
+    gasPrice: number
+}
+
+interface BlockInterface {
+    height: number,
+    timestamp: number,
+    seed: number
+}
+
+interface BlockchainInterface {
+    transaction: TransactionInterface,
+    block: BlockInterface,
+    verifyAddress: (string) => number,
+    getAccountState: (string) => any,
+    getPreBlockHash: (offset) => string,
+    getPreBlockSeed: (offset) => string,
+    transfer: (string, BigNumber) => boolean
+}
+
+interface EventInterface {
+    Trigger: (string, any) => void
+}
+
+// TODO: crypto
+
+declare var LocalContractStorage: LocalContractStorageInterface;
+declare var Blockchain: BlockchainInterface;
+declare var Event: EventInterface;
